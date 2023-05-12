a4a01a67538bcac49a156c2ec586658be53309a5
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2023-05-11 23:53:18 -0400

    practica corregida

--- Practica_2/arrays.js
+++ Practica_2/arrays.js
@@ -1,23 +1,4 @@
-/* let nombre = 'Harold';
-console.log('el nombre es',nombre);
-
-let nombres = ['Harold', 'Luis', 'Daniela', 'Maria', 'Alejandro'];
-//console.log('Los estudiantes son: ', nombres);
-//console.log('el estudiante :', nombres[1]);
-//Alejandro
-
-for (let i = 0; i < nombres.length; i++) {
-    console.log(nombres[i])
-}
-console.log('el tamaño es',nombres.length);
-let nombreLargo = [0];
-for (let i = 0; i < nombres.length; i++) { //i=1
-        if (nombreLargo.length <= nombres[i].length) { //6 <= 4
-            nombreLargo = nombres[i];
-        }
-}
-console.log('El nombre mas largo es: ', nombreLargo);
- */
+/*PREGUNTA FIBONACCI
 /* let n = 10;
 let a = 0;
 let b = 1;
@@ -34,15 +15,7 @@ for (let i = 0; i < n-2; i++) {
 console.log(fibonacci); */
 
 
-/* let nombre = 'Luis';
-let numero = 5;
-let plantas = ['rosas', 'claveles', 'margaritas'];
-console.log(plantas.length);
-for (let i = 0; i < plantas.length; i++) {
-    console.log('la planta: ' + plantas[i]);
-    console.log(`la planta : ${plantas[i]} y esta en la posicion Nº ${i}`);
-} */
-
+/*PREGUNTA COCACOLA CON ARRAYS
 /* let CocaCola = ['3lt', '2lt', '500ml', '300ml'];
 let precios = [13, 10, 5, 3];
 
@@ -52,35 +25,43 @@ for (let i = 0; i < CocaCola.length; i++) {
 }
  */
 
-let auto = 'Toyota';
+/* let auto = 'Toyota';
 let marcas = [];
-/* let arr = [
-    '{"marca" :"Nissan"}',
-    '{"marca" :"Honda"}',
-    '{"marca" :"Toyota"}',
-    '{"marca" :"Alfa Romero"}',
-    
-]; */
 let autos = ['Nissan', 'Honda', 'Ford', 'Toyota', 'Alfa Romero'];
 marcas.push(autos);
 let autoLargo = [0];
 for (let i = 0; i < autos.length; i++) { //i=1
         if (autoLargo.length <= autos[i].length) { //6 <= 4
             autoLargo = autos[i];
-        }
-        
+        }      
 }
-/* let series = arr.reduce((acc, cur) => {
-    // Interpretar JSON del elemento actual e insertar en acumulador
-    acc.push(JSON.parse(cur));
-    // Devolver acumulador
-    return acc;
-}, []); // El acumulador inicia como arreglo vacío
- 
-console.log(series);*/
 console.log('Autos = ',marcas);
 console.log(auto);
-console.log(autoLargo);
+console.log(autoLargo); */
+
+//numeros pares e impares 
+let array = [2 ,6 ,8,15,17, 22, 7 ,14]
+let nummayor=[0];
+let resultadopares=[];
+let   resultadoimpar=[];
+for(let i=0; i<array.length; i++){
+     if(nummayor<= array[i])
+     {
+        nummayor=array[i];
+     }
+}
+for(let i=0; i<array.length; i++)
+{
+   if  ((array [i]% 2) ==0)
+   {
+       resultadopares.push(array[i]);   
+    }
+    else
+    {
+     resultadoimpar.push(array[i]);
+    }
+}
 
 
-// Recorrer arreglo original, en cada iteración se recibe acumulador y elemento actual
+console.log('los numeros pares del array son  =',resultadopares);
+console.log('los numeros impares del array son= ',resultadoimpar);
